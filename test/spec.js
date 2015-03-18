2e556eaed3e12116a1c7442bb73dfcc772bf2104
Asbjørn Enge <asbjorn@hanafjedle.net>
2015-03-18 09:35:47 +0100

    Improved tests

--- test/spec.js
+++ test/spec.js
@@ -1,8 +1,35 @@
 var assert    = require('assert')
 var inspector = require('../index')({ hosts: [{ host : "127.0.0.1", port : 4243 }]})
+var broken    = require('../index')({ hosts: [{ host : "127.0.0.1", port : 4443 }]})
 
-it('can inspect all containers on a host', function(done) {
-    inspector.inspect(function(err, containers) {
+it('can inspect all containers across hosts', (done) => {
+    inspector.inspect((err, containers) => {
+        assert(err == null)
+        assert(containers instanceof Array)
+        assert(containers.length > 1)
         done()
     })
+})
+
+it('can inspect a single container id across hosts', (done) => {
+    inspector.inspect((err, containers) => {
+        assert(err == null)
+        assert(containers instanceof Array)
+        assert(containers.length == 1)
+        done()
+    }, 'dispatcher')
+})
+
+it('will return err if err for multiple', (done) => {
+    broken.inspect((err, containers) => {
+        assert(err != null)
+        done()
+    })
+})
+
+it('will return err if err for multiple', (done) => {
+    broken.inspect((err, containers) => {
+        assert(err != null)
+        done()
+    },'dispatcher')
 })
